97db706a8cc416a0e1213bc15fda10192b7c7f98
C0ld-m <imcold021@gmail.com>
2024-10-19 14:17:42 +0200

    wave system = done (kinda)

--- Assets/WaveSystem/Scripts/WaveSystem.cs
+++ Assets/WaveSystem/Scripts/WaveSystem.cs
@@ -12,19 +12,76 @@ public class WaveSystem : MonoBehaviour
     [Header("-----------SpawnInfos--->")]
     [SerializeField] private List<spawnInfo> spawnInfo;
 
+    [HideInInspector] public int aliveEnemys;
+
     //private vars...
     private int spawnPoint_ID;
     private int enemyPoint_ID;
-    private int wave_ID;
+    private int lastSpawnPoint_ID = -1;
+    private int wave_ID = -1;
 
-    private IEnumerator GenrateRandomePoints()
+    //temporary vars...
+    private bool nightTime = false;
+    private bool waveEnds = false;
+
+    private void Start()
     {
+        nightTime = true;
         wave_ID++;
+        aliveEnemys = 0;
+        StartCoroutine(EnemyGenerator());
+    }
+
+    private void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Space) && !nightTime)
+        {
+            nightTime = true;
+            wave_ID++;
+            aliveEnemys = 0;
+            StartCoroutine(EnemyGenerator());
+        }
+        */
+
+        if (aliveEnemys <= 0 && !nightTime && waveEnds)
+            Debug.Log("CONGRATULATIONA! U SURVIVED THIS NIGHT!");
+    }
+
+    private IEnumerator EnemyGenerator()
+    {
+        for (int i = 0; i < spawnInfo[wave_ID].amoutOfEnemys; i++)
+        {
+            GenrateRandomePoints();
+            yield return new WaitForSeconds(spawnInfo[wave_ID].timeBetweenSpawn);
+        }
+
+        //Debug.Log("Loop Ends");
+        
+        while (aliveEnemys > 0)
+        {
+            yield return null;
+        }
+
+        waveEnds = true;
+        nightTime = false;
+
+    }
+
+    private void GenrateRandomePoints()
+    {
         spawnPoint_ID = Random.Range(0, spawnPoints.Length);
+        if (spawnPoint_ID == lastSpawnPoint_ID)
+        {
+            spawnPoint_ID = (spawnPoint_ID + 1) % spawnPoints.Length; // Move to the next spawn point or wrap around
+        }
+
+        lastSpawnPoint_ID = spawnPoint_ID;
+
         enemyPoint_ID = Random.Range(0, spawnInfo[wave_ID].Enemyes.Length);
 
         Instantiate(spawnInfo[wave_ID].Enemyes[enemyPoint_ID], spawnPoints[spawnPoint_ID]);
-        yield return new WaitForSeconds(spawnInfo[wave_ID].timeBetweenWaves);
+        aliveEnemys++;
     }
 }
 
@@ -32,7 +89,8 @@ public class WaveSystem : MonoBehaviour
 public class spawnInfo
 {
     [field: SerializeField] public string wave {  get; private set; }
-    [field: SerializeField] public float timeBetweenWaves {  get; private set; }
+    [field: SerializeField] public float timeBetweenSpawn {  get; private set; }
+    [field: SerializeField] public int amoutOfEnemys {  get; private set; }
     [field: SerializeField] public GameObject[] Enemyes {  get; private set; }
 }
 
